1ede957ebd1f04ed1d2f5d9daf793ea735e94728
Joshua Layne <joshualayne246@gmail.com>
2025-09-03 01:05:25 -0400

    Update product_of_array_except_self.py

--- algorithms/arrays/product_of_array_except_self.py
+++ algorithms/arrays/product_of_array_except_self.py
@@ -1,3 +1,12 @@
+"""
+    Aim: To return an array where each element is a product of everything else except itself
+
+    Strategy:
+    * Start multiplying products from the left whereby the first will be effectively 1, and append to the left array so that it continually multiplies the previous product by the previous number. So in effect, it stores the product of all values to the current index's left
+    * Do the same for the right, but start from the other side
+    * Reverse the right, and multiply the elements of each in order
+"""
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         prods = []
@@ -5,13 +14,17 @@ class Solution:
         left = [1]
         right = [1]
 
-        for i in range(1, len(nums)):
+        for i in range(1, len(nums)): # determining left prefix
             currProd = left[i-1] * nums[i-1]
             left.append(currProd)
 
-        for i in range(len(nums) - 1, 0, -1):
+        #print(left)
+
+        for i in range(len(nums) - 1, 0, -1): # determining right suffix
             currProd = right[len(nums) - i - 1] * nums[i]
             right.append(currProd)
+
+        #print(right)
 
         right.reverse()
 
