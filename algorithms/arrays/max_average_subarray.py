e977e1c709869152172870ca43e8f4e75ae8ffd7
Joshua Layne <joshualayne246@gmail.com>
2025-09-05 13:18:48 -0400

    Update max_average_subarray.py

--- algorithms/arrays/max_average_subarray.py
+++ algorithms/arrays/max_average_subarray.py
@@ -1,3 +1,12 @@
+"""
+Aim: To find a contiguous subarray with length k that has the maximum average, and return said average
+
+Strategy:
+* use a fixed sliding window (with the sum of said window saved and calcuate the initial average)
+* moving right, add the next value in the array and remove the first in the window, and recalculate the average
+* check to see if it is greater than the maximum at each point
+"""
+
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         window_sum = sum(nums[:k])
