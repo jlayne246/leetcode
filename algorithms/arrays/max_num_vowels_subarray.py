9d96e87fbf45374d75bd52c796077857630f50f8
Joshua Layne <joshualayne246@gmail.com>
2025-09-05 13:20:36 -0400

    Update max_num_vowels_subarray.py

--- algorithms/arrays/max_num_vowels_subarray.py
+++ algorithms/arrays/max_num_vowels_subarray.py
@@ -1,3 +1,10 @@
+"""
+Aim: To calculate the maximum number of vowels in any substring with the length k
+
+Strategy:
+* Sliding window: Count the number of vowels in the window space, and keep moving the window across the array (removing any that are taken out) and calculate the maximum in each iteration.
+"""
+
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         vowels = ['a', 'e', 'i', 'o', 'u']
